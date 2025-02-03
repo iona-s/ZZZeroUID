0874b0ca7d797047066a828ba8792dd4134a9dbe
iona <724207289@qq.com>
2025-02-03 21:11:12 +0800

    ✨ 体力提醒添加更多信息

--- ZZZeroUID/zzzerouid_stamina/notice.py
+++ ZZZeroUID/zzzerouid_stamina/notice.py
@@ -48,67 +48,65 @@ async def all_check(
     user_id: str,
     uid: str,
 ) -> Dict[str, Dict[str, Dict]]:
-    _check = await check(
+    check_result = await check(
         raw_data,
         push_data.get("energy_value", 0),
     )
 
     # 检查条件
-    if push_data["energy_is_push"] == "on":
+    if push_data["energy_push"] == "off":
+        return msg_dict
+
+    if not check_result:
         if not ZZZ_CONFIG.get_config("CrazyNotice").data:
-            if not _check:
-                await ZzzPush.update_data_by_uid(
-                    uid, bot_id, "zzz", **{"energy_is_push": "off"}
-                )
+            await ZzzPush.update_data_by_uid(
+                uid, bot_id, "zzz", **{"energy_is_push": "off"}
+            )
+        return msg_dict
 
     # 准备推送
-    if _check:
-        if push_data["energy_push"] == "off":
-            pass
+    if bot_id not in msg_dict:
+        msg_dict[bot_id] = {"direct": {}, "group": {}}
+    direct_data = msg_dict[bot_id]["direct"]
+    group_data = msg_dict[bot_id]["group"]
+
+    # on 推送到私聊
+    if push_data["energy_push"] == "on":
+        # 添加私聊信息
+        if user_id not in direct_data:
+            direct_data[user_id] = check_result
         else:
-            # 达到提醒
-            if _check == 'warn':
-                notice = '[绝区零] 你的电量已达提醒阈值！' + ZZZ_NOTICE
-            # 已满
-            if _check == 'full':
-                notice = '[绝区零] 你的电量已满！' + ZZZ_NOTICE
-            # 初始化
-            if bot_id not in msg_dict:
-                msg_dict[bot_id] = {"direct": {}, "group": {}}
-                direct_data = msg_dict[bot_id]["direct"]
-                group_data = msg_dict[bot_id]["group"]
+            direct_data[user_id] += check_result
+    # 群号推送到群聊
+    else:
+        # 初始化
+        gid = push_data["energy_push"]
+        if gid not in group_data:
+            group_data[gid] = {}
 
-            # on 推送到私聊
-            if push_data["energy_push"] == "on":
-                # 添加私聊信息
-                if user_id not in direct_data:
-                    direct_data[user_id] = notice
-                else:
-                    direct_data[user_id] += notice
-            # 群号推送到群聊
-            else:
-                # 初始化
-                gid = push_data["energy_push"]
-                if gid not in group_data:
-                    group_data[gid] = {}
+        if user_id not in group_data[gid]:
+            group_data[gid][user_id] = check_result
+        else:
+            group_data[gid][user_id] += check_result
 
-                if user_id not in group_data[gid]:
-                    group_data[gid][user_id] = notice
-                else:
-                    group_data[gid][user_id] += notice
-
-            await ZzzPush.update_data_by_uid(
-                uid, bot_id, 'zzz', **{"energy_is_push": "on"}
-            )
-    return msg_dict
+    await ZzzPush.update_data_by_uid(
+        uid, bot_id, 'zzz', **{"energy_is_push": "on"}
+    )
 
 
 async def check(data: ZZZNoteResp, limit: int) -> str:
-    # 不知道为什么不能用data.xx来取值
-    current = data["energy"]['progress']['current']
-    max = data["energy"]['progress']['max']
-    if current >= max:
-        return 'full'
+    energy_data = data["energy"]
+    progress = energy_data["progress"]
+    current = progress["current"]
+    base_notice = '[绝区零] 你的电量'
+    if current >= progress["max"]:
+        return base_notice + "已满！" + ZZZ_NOTICE
     if current >= limit:
-        return 'warn'
+        current_status = f"当前{current}/{progress['max']}，将于"
+        if energy_data["day_type"] == 1:
+            current_status += "今"
+        else:
+            current_status += "明"
+        current_status += f"日{energy_data['hour']}:{energy_data['minute']}回满"
+        return base_notice + "已达提醒阈值！\n" + current_status + ZZZ_NOTICE
     return ''
